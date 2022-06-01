65c3ed82bc92e9d1a3a0000b91938ef5bd4f2a0f
BoD <BoD@JRAF.org>
2022-06-01 11:46:23 +0200

    Add a case without generics

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,11 @@
 package com.example;
 
 import com.apollographql.apollo3.ApolloCall;
+import com.apollographql.apollo3.api.Operation;
 
 public class Main {
     public static void main(String[] av) {
+        Operation.Data data = new MyQuery.Data();
         ApolloCall<MyQuery.Data> queryCall = null;
     }
 }
